d41272a279c78a87380f8d9702865e87e330a9fa
Ayesha116 <ayeshajawed1168@gmail.com>
2021-01-28 23:19:59 +0500

    service worker work successfull

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyAvlmoVcDGSTU7MqcOCnWR5frevmyzcV64",
+    authDomain: "pwa-practice-8d5ce.firebaseapp.com",
+    projectId: "pwa-practice-8d5ce",
+    storageBucket: "pwa-practice-8d5ce.appspot.com",
+    messagingSenderId: "434024461969",
+    appId: "1:434024461969:web:3841eda935aa0255f03130"
+}
+firebase.initializeApp(config)
+
+export default firebase;
